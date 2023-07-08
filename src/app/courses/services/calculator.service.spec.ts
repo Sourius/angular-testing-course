2552ff8e392db8cfbc3aa1b70cde8ef76d451234
sourius <Sourius@users.noreply.github.com>
2023-07-08 12:39:36 +0200

    update test: add courses service test with httpTestingController

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -7,10 +7,9 @@ describe('Calculator Service', () => {
     let loggerSpy: LoggerService;
 
     beforeEach(() => {
-        console.log("Start before each")
         // spyOn(logger, 'log');
         loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-        
+
         TestBed.configureTestingModule({
             providers: [
                 CalculatorService,
@@ -23,23 +22,17 @@ describe('Calculator Service', () => {
 
         // calculator = new CalculatorService(loggerSpy);
         calculator = TestBed.inject(CalculatorService);
-
-        console.log("End before each")
     });
 
     it('should add two numbers', () => {
-        console.log("Start add two numbers")
         const result = calculator.add(2, 2);
         expect(result).toBe(4, "unexpected add result");
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-        console.log("End add two numbers")
     });
 
     it('should subtract two numbers', () => {
-        console.log("Start subtract two numbers")
         expect(calculator.subtract(3, 2)).toBe(1, 'unexpected substraction result');
         expect(calculator.subtract(1, 3)).toBe(-2, 'unexpected substraction result');
         expect(loggerSpy.log).toHaveBeenCalledTimes(2);
-        console.log("End subtract two numbers")
     });
 });
