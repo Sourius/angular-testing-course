fe83aea6019b6b4b96be2149d1015bf391e9807a
sourius <Sourius@users.noreply.github.com>
2023-07-08 09:39:13 +0200

    update test: before each

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,22 +2,30 @@ import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
 describe('Calculator Service', () => {
-    
+    let calculator: CalculatorService;
+    let loggerSpy: LoggerService;
+
+    beforeEach(() => {
+        console.log("Start before each")
+        // spyOn(logger, 'log');
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+        calculator = new CalculatorService(loggerSpy);
+        console.log("End before each")
+    });
+
     it('should add two numbers', () => {
-        const logger = new LoggerService();
-        const calculatorService = new CalculatorService(logger);
-        spyOn(logger, 'log');
-        const result = calculatorService.add(2,2);
+        console.log("Start add two numbers")
+        const result = calculator.add(2, 2);
         expect(result).toBe(4, "unexpected add result");
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+        console.log("End add two numbers")
     });
 
     it('should subtract two numbers', () => {
-        const logger = new LoggerService();
-        const calculatorService = new CalculatorService(logger);
-        spyOn(logger, 'log');
-        expect(calculatorService.subtract(3,2)).toBe(1, 'unexpected substraction result');
-        expect(calculatorService.subtract(1,3)).toBe(-2, 'unexpected substraction result');
-        expect(logger.log).toHaveBeenCalledTimes(2);
+        console.log("Start subtract two numbers")
+        expect(calculator.subtract(3, 2)).toBe(1, 'unexpected substraction result');
+        expect(calculator.subtract(1, 3)).toBe(-2, 'unexpected substraction result');
+        expect(loggerSpy.log).toHaveBeenCalledTimes(2);
+        console.log("End subtract two numbers")
     });
 });
